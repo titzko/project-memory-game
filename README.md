a784f7a7500b4aac0de851d0c114afecd3f79a79
titzko <matitzko@gmail.com>
2023-06-08 11:19:57 +0200

    Some styling, showing spinner untill async calls finished

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 made with
 - bootstrap
 - [pokemon-api](https://github.com/PokeAPI/pokeapi-js-wrapper)
-
+- [react-loading-spinner](https://www.npmjs.com/package/react-loader-spinner)
 
 find a running instance [here](https://titzko.github.io/project-memory-game/)
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,42 +1,46 @@
-import React, { useState, useEffect } from "react"
-
-const Pokedex = require("pokeapi-js-wrapper");
-const P = new Pokedex.Pokedex();
-
+import React, { useState, useEffect } from 'react';
+import { BallTriangle } from 'react-loader-spinner';
 
 export default function Card({ name, onCardClick }) {
-
     const [pokemon, setPokemon] = useState({});
-    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        const getPokemonByName = async () => {
-            try {
-                const pokemon = await P.getPokemonByName(name);
-                setPokemon(pokemon);
-            } catch (error) {
-                setError(error);
-            }
+        const Pokedex = require('pokeapi-js-wrapper');
+        const P = new Pokedex.Pokedex();
+
+        async function getPokemonByName() {
+            return await P.getPokemonByName(name);
         }
 
-        getPokemonByName();
+        getPokemonByName().then(p => {
+            setPokemon(p);
+            setIsLoading(false);
+        });
 
-    }, [name])
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+    }, []);
 
     return (
-        <div className="card text-white bg-dark" onClick={onCardClick}>
+        <div className="card text-white bg-dark" onClick={onCardClick} style={{ "height": "200px" }}>
             <h5 className="card-title d-flex justify-content-center mt-3">{pokemon.name}</h5>
-            {pokemon.sprites &&
-                <img
-                    src={pokemon.sprites.back_default}
-                    alt="pokemon sprite"
-                    style={{ width: "150px", height: "150px" }}
-                />
-            }
+            {isLoading ? (
+                <div className='d-flex justify-content-center align-items-center h-100'>
+                    <BallTriangle
+                        color="#00BFFF"
+                        height={50}
+                        width={50}
+                        timeout={3000}
+                    />
+                </div>
+            ) : (
+                pokemon.sprites && (
+                    <img
+                        src={pokemon.sprites.back_default}
+                        alt="pokemon sprite"
+                        style={{ width: "150px", height: "150px" }}
+                    />
+                )
+            )}
         </div>
-    )
+    );
 }
